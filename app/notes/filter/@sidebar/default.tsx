5fcbe4e5b7e1719693acf8c3b122bd8d6c860de4
Roksolana Fedyna <roksolanayatsyshyn@gmail.com>
2026-01-14 12:50:53 +0200

    React Query add

--- app/notes/filter/@sidebar/default.tsx
+++ app/notes/filter/@sidebar/default.tsx
@@ -1,10 +1,8 @@
-import SidebarNotesClient from './SidebarNotes.client.tsx';
+import SidebarNotesClient from './SidebarNotes.client';
 
 export const dynamic = 'force-dynamic';
 
 export default function Sidebar() {
   return <SidebarNotesClient />;
-  // return (
-  //   <h1 style={{ color: "red" }}>SIDEBAR</h1>
-  // );
+  
 }
